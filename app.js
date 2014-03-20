970703ebfbe44b908493fccfdc14ea3d69f92bbc
Jesse Shawl <jesse@jshawl.com>
2014-03-20 13:15:20 -0400

    Add less compiler

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var less = require('less');
 var app = express();
 
-app.get('/', function( req, res ){
-  res.send('workin');
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/less', function( req, res ){
+  var less_input = req.body.less;
+  less.render(less_input, function( e, css ){
+    if (e) throw e;
+    res.send(css);
+  });
 });
 
 app.listen(3000, function(){
